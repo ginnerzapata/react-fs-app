9cbb8d190a87cb63f6e08962b7d959a456ec6475
Ginner Zapata <gzapata@vizzn.ca>
2021-11-01 05:11:36 -0300

    using react-router links

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AboutPage from './pages/AboutPage'
 import ArticlesList from './pages/ArticlesList'
 import ArticlePage from './pages/ArticlePage'
+import NavBar from './NavBar'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/articles-list" component={ArticlesList} />
-        <Route path="/article" component={ArticlePage} />
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   )
