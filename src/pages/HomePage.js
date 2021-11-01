14e235e2cd62c62c2418e6d789232fc9c4e5196e
Ginner Zapata <gzapata@vizzn.ca>
2021-11-01 04:58:53 -0300

    creating the app component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Hello, welcome to my blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue
+        rhoncus diam non aliquam. Donec commodo felis ligula, eget commodo justo
+        tristique sed. Pellentesque lacinia, velit at bibendum rutrum, turpis
+        purus venenatis lorem, ut vestibulum dui metus sit amet massa. Donec
+        eget pretium purus, non pretium tellus. Cras ultrices ligula hendrerit
+        enim tristique finibus. Nunc finibus lectus a pretium tristique.
+        Vestibulum elementum non purus ac ultricies. Duis id erat facilisis,
+        lobortis risus at, laoreet velit. Integer sapien justo, vehicula sed
+        nisi in, faucibus vehicula dolor. Mauris at magna justo. Sed pretium ut
+        sem sed mollis. Integer a ante diam. Etiam at lacus massa. Vestibulum ut
+        tempor urna, nec fringilla purus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue
+        rhoncus diam non aliquam. Donec commodo felis ligula, eget commodo justo
+        tristique sed. Pellentesque lacinia, velit at bibendum rutrum, turpis
+        purus venenatis lorem, ut vestibulum dui metus sit amet massa. Donec
+        eget pretium purus, non pretium tellus. Cras ultrices ligula hendrerit
+        enim tristique finibus. Nunc finibus lectus a pretium tristique.
+        Vestibulum elementum non purus ac ultricies. Duis id erat facilisis,
+        lobortis risus at, laoreet velit. Integer sapien justo, vehicula sed
+        nisi in, faucibus vehicula dolor. Mauris at magna justo. Sed pretium ut
+        sem sed mollis. Integer a ante diam. Etiam at lacus massa. Vestibulum ut
+        tempor urna, nec fringilla purus.
+      </p>
+    </>
+  )
+}
+
+export default HomePage
