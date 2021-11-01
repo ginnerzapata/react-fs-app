aadecfe4ff16be1af11efef4e24b39ea345ff1c8
Ginner Zapata <gzapata@vizzn.ca>
2021-11-01 05:06:56 -0300

    creating blog pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
 import HomePage from './pages/HomePage'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import ArticlesList from './pages/ArticlesList'
+import ArticlePage from './pages/ArticlePage'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route path="/" component={HomePage} exact />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/articles-list" component={ArticlesList} />
+        <Route path="/article" component={ArticlePage} />
       </div>
     </Router>
   )
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ArticlesList = () => {
+  return <h1>Articles</h1>
+}
+
+export default ArticlesList
